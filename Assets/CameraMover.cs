3e7a3ac86514793402eb8ba062957d9f94533651
faustxp8 <faustxp8@gmail.com>
2015-04-16 17:37:40 +0300

    box

---
+++ Assets/CameraMover.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMover : MonoBehaviour {
+
+	public float targetY = 0;
+	public float pozitia;
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = transform.position;
+		pozitia = transform.position.y ;
+		pos.y = Mathf.Lerp(transform.position.y, targetY, 1 * Time.deltaTime);
+		transform.position = pos;
+	}
+}
---
+++ Assets/Position.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Position : MonoBehaviour {
+	void Start()
+	{
+		Vector3 pos = transform.position;
+		pos.y =-2;
+		transform.position = pos;
+	}
+	
+	void Update () {
+		
+		Vector3 pos = transform.position;
+		pos.y = GameObject.FindObjectOfType< CameraMover > ().pozitia - 3;
+		transform.position = pos;
+	}
+}
